703910a1fba33211a4370b7cfee20cf99123e7c5
MartenF <marten-fahse@hotmail.de>
2014-05-21 18:31:46 +0200

    init

---
+++ X10Reciever.c
@@ -0,0 +1,17 @@
+/*
+ * X10Reciever.c
+ *
+ * Created: 21.05.2014 18:30:49
+ *  Author: betabot
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
