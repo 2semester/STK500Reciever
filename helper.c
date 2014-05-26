9d8f1570f7daeb609707c870a988aba916ee41e3
zyprezz <zyprezz@gmail.com>
2014-05-26 23:01:34 +0200

    bitconversion error correted

--- helper.c
+++ helper.c
@@ -20,10 +20,10 @@ void shortenstring(char** buffer, char* DataBuffer){
 	*buffer[1] = '0';
 	if(DataBuffer[5] == 0 && DataBuffer[6] == 0 && DataBuffer[7] == 0 && DataBuffer[3] == 1 && DataBuffer[5] == 1) 
 	*buffer[1] = '1';
-	if(DataBuffer[5] == 0 && DataBuffer[6] == 0 && DataBuffer[7] == 0 && DataBuffer[8] == 0 && DataBuffer[9] == 0) 
+	if(DataBuffer[5] == 0 && DataBuffer[6] == 0 && DataBuffer[7] == 1 && DataBuffer[8] == 0 && DataBuffer[9] == 0) 
 	*buffer[1] = '2';// '2'
 	if(DataBuffer[5] == 0 && DataBuffer[6] == 0 && DataBuffer[7] == 1 && DataBuffer[8] == 0 && DataBuffer[9] == 1) 
-	*buffer[1] = '3';// '3'7
+	*buffer[1] = '3';// '3'
 	if(DataBuffer[5] == 0 && DataBuffer[6] == 0 && DataBuffer[7] == 1 && DataBuffer[8] == 1 && DataBuffer[9] == 0) 
 	*buffer[1] = '4';// '4'
 	if(DataBuffer[5] == 0 && DataBuffer[6] == 0 && DataBuffer[7] == 1 && DataBuffer[8] == 1 && DataBuffer[9] == 1) 
@@ -36,7 +36,7 @@ void shortenstring(char** buffer, char* DataBuffer){
 	*buffer[1] = '8';// '8'
 	if(DataBuffer[5] == 0 && DataBuffer[6] == 1 && DataBuffer[7] == 0 && DataBuffer[8] == 1 && DataBuffer[9] == 1) 
 	*buffer[1] = '9';// '9'
-	if(DataBuffer[5] == 0 && DataBuffer[6] == 1 && DataBuffer[7] == 0 && DataBuffer[8] == 0 && DataBuffer[9] == 0) 
+	if(DataBuffer[5] == 0 && DataBuffer[6] == 1 && DataBuffer[7] == 1 && DataBuffer[8] == 0 && DataBuffer[9] == 0) 
 	*buffer[1] = 'a';// 'a'
 	if(DataBuffer[5] == 0 && DataBuffer[6] == 1 && DataBuffer[7] == 1 && DataBuffer[8] == 0 && DataBuffer[9] == 1) 
 	*buffer[1] = 'b';// 'b'
@@ -44,15 +44,15 @@ void shortenstring(char** buffer, char* DataBuffer){
 	*buffer[1] = 'c';// 'c'
 	if(DataBuffer[5] == 0 && DataBuffer[6] == 1 && DataBuffer[7] == 1 && DataBuffer[8] == 1 && DataBuffer[9] == 1) 
 	*buffer[1] = 'd';// 'd'
-	if(DataBuffer[5] == 1 && DataBuffer[6] == 0 && DataBuffer[7] == 1 && DataBuffer[8] == 0 && DataBuffer[9] == 0) 
+	if(DataBuffer[5] == 1 && DataBuffer[6] == 0 && DataBuffer[7] == 0 && DataBuffer[8] == 0 && DataBuffer[9] == 0)
 	*buffer[1] = 'e';// 'e'
 	if(DataBuffer[5] == 1 && DataBuffer[6] == 0 && DataBuffer[7] == 0 && DataBuffer[8] == 0 && DataBuffer[9] == 1) 
 	*buffer[1] = 'f';// 'f'
 	if(DataBuffer[10] == 0 && DataBuffer[11] == 0 && DataBuffer[12] == 0 && DataBuffer[13] == 1 && DataBuffer[14] == 0) 
 	*buffer[2] = 'f'; // on
-	if(DataBuffer[10] == 1 && DataBuffer[11] == 0 && DataBuffer[12] == 0 && DataBuffer[13] == 0 && DataBuffer[14] == 1) 
+	if(DataBuffer[10] == 1 && DataBuffer[11] == 0 && DataBuffer[12] == 0 && DataBuffer[13] == 1 && DataBuffer[14] == 1) 
 	*buffer[2] = '0';// off
-	if(DataBuffer[15] == 1 && DataBuffer[16] == 0 && DataBuffer[17] == 0 && DataBuffer[18] == 1 && DataBuffer[19] == 0) 
+	if(DataBuffer[15] == 1 && DataBuffer[16] == 0 && DataBuffer[17] == 1 && DataBuffer[18] == 0 && DataBuffer[19] == 0)
 	*buffer[3] = '\r';// '\r'
 	*buffer[4] = '\0';
 }
